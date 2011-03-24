53767399548dc0bf561974f72fe00a086653c159
Sander Hartlage <sander@animoto.com>
2011-03-24 14:59:45 -0400

    added some missing specs

--- spec/animoto/assets/base_spec.rb
+++ spec/animoto/assets/base_spec.rb
@@ -2,4 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
 
 describe Animoto::Assets::Base do
   
+  describe "initialization" do
+    before do
+      @source = 'http://website.com/asset'
+      @asset = Animoto::Assets::Base.new @source
+    end
+    
+    it "should set the source from the given source url" do
+      @asset.source.should == @source
+    end
+  end
+  
+  describe "#to_hash" do
+    before do
+      @asset = Animoto::Assets::Base.new 'http://website.com/asset'
+    end
+    
+    it "should have a 'source_url' key with the url" do
+      @asset.to_hash.should have_key('source_url')
+      @asset.to_hash['source_url'].should == @asset.source
+    end
+  end
+  
 end
--- spec/animoto/resources/storyboard_spec.rb
+++ spec/animoto/resources/storyboard_spec.rb
@@ -1,8 +1,54 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
 
 describe Animoto::Resources::Storyboard do
-  def storyboard options = {}
-    @storyboard ||= Animoto::Resources::Storyboard.new options
+
+  it "should have content type 'application/vnd.animoto.storyboard'" do
+    Animoto::Resources::Storyboard.content_type.should == 'storyboard'
+  end
+  
+  it "should have payload key 'storyboard'" do
+    Animoto::Resources::Storyboard.payload_key.should == 'storyboard'
+  end
+
+  describe "initialization" do
+    before do
+      @body = {
+        'response' => {
+          'status' => {
+            'code' => 200
+          },
+          'payload' => {
+            'storyboard' => {
+              'metadata' => {
+                'duration' => 300.0,
+                'visuals_count' => 60
+              },
+              'links' => {
+                'self' => 'https://platform.animoto.com/storyboards/1',
+                'preview' => 'http://storage.com/previews/1.mp4'
+              }
+            }
+          }
+        }
+      }
+      @storyboard = Animoto::Resources::Storyboard.load(@body)
+    end
+    
+    it "should set its url from the 'self' link given" do
+      @storyboard.url.should == 'https://platform.animoto.com/storyboards/1'
+    end
+    
+    it "should set its preview url from the 'preview' link given" do
+      @storyboard.preview_url.should == 'http://storage.com/previews/1.mp4'
+    end
+    
+    it "should set its duration from the 'duration' metadata given" do
+      @storyboard.duration.should == 300.0
+    end
+    
+    it "should set its visuals count from the visuals_count metadata given" do
+      @storyboard.visuals_count.should == 60
+    end
   end
   
 end
