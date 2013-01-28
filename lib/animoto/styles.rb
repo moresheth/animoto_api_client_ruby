5fddee9390fa266429cbfaf362782ddf685428bf
Jeff Kahsen <jeff.kahsen@animoto.com>
2013-01-28 09:58:39 -0800

    Updated Styles Enum and bumped version

--- lib/animoto/styles.rb
+++ lib/animoto/styles.rb
@@ -1,40 +1,47 @@
 module Animoto
-  module Styles
-
-    ANIMOTO_ORIGINAL      = "original"
-    WATERCOLOR_SEASHORE   = "mothers_day_2011"
-    DUSK_RETREAT          = "elegance_12"
-    VINTAGE_VOYAGE        = "vintage"
-    COSMIC_TIDINGS        = "cosmic_tidings"
-    WONDERLAND_OF_SNOW    = "wonderland_of_snow"
-    THROUGH_THE_BLOSSOMS  = "valentines_day_2012_hands"
-	WATER				  = "elements_water"
-	SIMPLICITY			  = "slideshow_1"
-	AIR				      = "elements_air"
-	FIRE				  = "elements_fire"
-	EARTH				  = "elements_earth"
-	COLOR_FOLD			  = "Cubist_style"
-	COMING_UP_ROSES		  = "valentines_2011"
-	WRAPPING_SCRAPS		  = "xmas-2010-style1"
-	THE_WINDING_VINE	  = "mothers_day"
-	POPUP_PANDEMONIUM	  = "holiday_popup_pandemonium"
-	STARRY_NIGHT		  = "holiday_starry_night"
-	RETRO_WHEEL			  = "viewmaster"
-	
-    DEPRECATED_NAMES = {
-      :ORIGINAL         => :ANIMOTO_ORIGINAL,
-      :MOTHERS_DAY_2011 => :WATERCOLOR_SEASHORE,
-      :ELEGANCE         => :DUSK_RETREAT,
-      :VINTAGE          => :VINTAGE_VOYAGE
-    }
-
-    def self.const_missing old_name
-      if new_name = DEPRECATED_NAMES[old_name]
+    module Styles
+        
+        ANIMOTO_ORIGINAL      = "original"
+        WATERCOLOR_SEASHORE   = "mothers_day_2011"
+        DUSK_RETREAT          = "elegance_12"
+        VINTAGE_VOYAGE        = "vintage"
+        COSMIC_TIDINGS        = "cosmic_tidings"
+        WONDERLAND_OF_SNOW    = "wonderland_of_snow"
+        THROUGH_THE_BLOSSOMS  = "valentines_day_2012_hands"
+        WATER				  = "elements_water"
+        SIMPLICITY			  = "slideshow_1"
+        AIR				      = "elements_air"
+        FIRE				  = "elements_fire"
+        EARTH				  = "elements_earth"
+        COLOR_FOLD			  = "Cubist_style"
+        COMING_UP_ROSES		  = "valentines_2011"
+        WRAPPING_SCRAPS		  = "xmas-2010-style1"
+        THE_WINDING_VINE	  = "mothers_day"
+        POPUP_PANDEMONIUM	  = "holiday_popup_pandemonium"
+        STARRY_NIGHT		  = "holiday_starry_night"
+        RETRO_WHEEL			  = "viewmaster"
+        AUTUMN                = "autumn"
+        PARTY                 = "party"
+        VINTAGE_INK           = "vintage_ink"
+        FIREWORKS             = "fireworks"
+        BRILLIANCE            = "elegant"
+        EVENTS                = "events"
+        LAND_OF_THE_RISING_HEARTS = "land_of_the_rising_hearts"
+        
+        DEPRECATED_NAMES = {
+        :ORIGINAL         => :ANIMOTO_ORIGINAL,
+        :MOTHERS_DAY_2011 => :WATERCOLOR_SEASHORE,
+        :ELEGANCE         => :DUSK_RETREAT,
+        :VINTAGE          => :VINTAGE_VOYAGE
+        }
+        
+        def self.const_missing old_name
+        if new_name = DEPRECATED_NAMES[old_name]
         warn("The style name \"#{old_name.to_s}\" is deprecated. Use \"#{new_name}\" instead.")
         const_set(old_name, const_get(new_name))
-      else
+        else
         super
-      end
     end
-  end
+end
+end
 end
