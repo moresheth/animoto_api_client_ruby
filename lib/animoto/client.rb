9fa74916d3d4a6b4403e360a69efcc9ca9b48572
srivenupaturi <srivenu_paturi@yahoo.com>
2013-06-18 18:02:52 -0400

    work in progress

--- lib/animoto/client.rb
+++ lib/animoto/client.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'ruby-debug'
 require 'uri'
 require 'logger'
 
--- lib/animoto/manifests/base.rb
+++ lib/animoto/manifests/base.rb
@@ -28,14 +28,19 @@ module Animoto
       # @return [String]
       attr_accessor :http_callback_format
 
+      attr_accessor :partner_metadata
+
       # Creates a new manifest
       #
       # @param [Hash{Symbol=>Object}] options
       # @option options [String] :http_callback_url a URL to receive a callback when this job is done
       # @option options [String] :http_callback_format the format of the callback
       def initialize options = {}
+debugger
         @http_callback_url  = options[:http_callback_url]
         @http_callback_format = options[:http_callback_format]
+        @partner_metadata  = {}
+        @partner_metadata["commercial_use"] = "Our=================== Best of 2013"
       end
 
       # Returns a representation of this manifest as a Hash, used to populate
@@ -55,6 +60,12 @@ module Animoto
       end
       
       private
+
+      def add_partner_metadata job_hash
+        if partner_metadata
+          job_hash['partner_metadata'] = partner_metadata
+        end
+      end
       
       # Helper method to put the standard HTTP callback information into the manifest hash
       #
